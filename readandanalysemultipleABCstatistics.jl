0802aef84ba396fb349b083b692d0a7375e21ec1
Mosè Giordano <m.giordano@ucl.ac.uk>
2024-07-30 13:44:09 +0100

    Minor improvements to read analysis script

--- readandanalysemultipleABCstatistics.jl
+++ readandanalysemultipleABCstatistics.jl
@@ -16,10 +16,11 @@ function readandanalysemultipleABCstatistics()
     withgraphical::Bool = true                  # 'true', if graphical output of joint analysis, 'false' otherwise
     withrotationcorrection::Bool = true         # 'true', if angles are rotated to give smaller variance, 'false' otherwise
     mode::Int64 = 2                             # '1' for using same trunkfilename throughout and vary suffix; '2' for individual filenames
-    filenames::Array{String,1} = fill("",0)     # initialise empty list of fullfilenames
+    filenames::Vector{String} = String[]        # initialise empty list of fullfilenames
     if( mode==1 )                               # shared trunkfilename
-        readchains::Array{Int64,1} = [ 1, 2, 3 ]    # indices of files to be read (in same order as they will be read)
-        trunkfilename::String = "2024-07-01_09-26-53_LineageMCoutput_(clock-GE-model_for_m2extrasmalltestdata)"; suffix::Array{String,1} = [@sprintf("(%d)",j_chain) for j_chain=readchains]
+        readchains::Vector{Int64} = [ 1, 2, 3 ]    # indices of files to be read (in same order as they will be read)
+        trunkfilename::String = "2024-07-01_09-26-53_LineageMCoutput_(clock-GE-model_for_m2extrasmalltestdata)"
+        suffix::Vector{String} = [@sprintf("(%d)",j_chain) for j_chain=readchains]
         for j_chain in eachindex(readchains)
             push!(filenames, @sprintf( "%s_%s.txt", trunkfilename,suffix[j_chain] ) )
         end     # end of chains loop
@@ -31,14 +32,12 @@ function readandanalysemultipleABCstatistics()
         @printf( " Warning - readandanalysemultipleABCstatistics: Unknown mode %d.\n", mode )
     end     # end of distinguishing modes
     @printf( " Info - readandanalysemultipleABCstatistics: Start with postprocessing existing file now, mode %d (after %1.3f sec).\n", mode, (DateTime(now())-t1)/Millisecond(1000) )
-    # read first line to decide which version:
-    local version::Float64                      # declare
-    open( filenames[1] ) do myfile              # assume version does not change between files
-        newline = readline(myfile)              # reads first line with version
-        whatitis = findfirst( "version:     \t",newline );  version = parse(Float64, newline[(whatitis[end]+1):lastindex(newline)] )
-    end     # end of file
+    # Read the first line of the file to decide the version.  First line of the file is of the format:
+    #     version:   3
+    # so we read it, split by ':' and parse the second part of the line.
+    version = parse(Int, split(readline(filenames[1]), ':')[2])
     if( version==3 )
-        (state_chains_hist::Array{Array{Lineagestate2,1},1},logprob_chains::Array{Float64,1}, uppars_chains::Array{Uppars2,1}, lineagetree::Lineagetree) = ABCreadmultiplestatesfromtexts( filenames )[[1,3,4,5]]
+        (state_chains_hist::Vector{Vector{Lineagestate2}}, logprob_chains::Vector{Float64}, uppars_chains::Vector{Uppars2}, lineagetree::Lineagetree) = ABCreadmultiplestatesfromtexts( filenames )[[1,3,4,5]]
     else                                        # unknown version
         @printf( " Warning - readandanalysemultipleABCstatistics: Unkonwn version %d.\n", version )
         error( " Error - readandanalysemultipleABCstatistics: Unknown version." )
