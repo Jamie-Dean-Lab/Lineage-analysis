ff41430345eaca8bc274a9dbe04be06115547a4a
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2024-10-02 15:23:10 +0100

    use cell in names rather than tracks

--- track_converter/src/preprocess_ctc.py
+++ track_converter/src/preprocess_ctc.py
@@ -17,7 +17,7 @@ def validate_tracks_shape_dtypes(tracks: pd.DataFrame) -> None:
         raise ValueError(msg)
 
     if nrows == 0:
-        msg = "input file must contain at least one track"
+        msg = "input file must contain at least one tracked cell"
         raise ValueError(msg)
 
     # add right-censoring column (if it doesn't exist), defaulting to all zero
@@ -34,15 +34,15 @@ def validate_tracks_shape_dtypes(tracks: pd.DataFrame) -> None:
         raise ValueError(msg)
 
     if not (tracks["L"] > 0).all():
-        msg = "all track labels (first column) must be greater than zero"
+        msg = "all tracked cell labels (first column) must be greater than zero"
         raise ValueError(msg)
 
     if not tracks["L"].is_unique:
-        msg = "all track labels (first column) must be unique"
+        msg = "all tracked cell labels (first column) must be unique"
         raise ValueError(msg)
 
     if not (tracks["P"] >= 0).all():
-        msg = "all track parent labels (fourth column) must be greater than or equal to zero"
+        msg = "all parent labels (fourth column) must be greater than or equal to zero"
         raise ValueError(msg)
 
     if not tracks["R"].isin((0, 1)).all():
@@ -50,9 +50,9 @@ def validate_tracks_shape_dtypes(tracks: pd.DataFrame) -> None:
         raise ValueError(msg)
 
 
-def validate_track_begin_end_frames(tracks: pd.DataFrame) -> None:
+def validate_cell_begin_end_frames(tracks: pd.DataFrame) -> None:
     """
-    Validate the begin and end frames of the tracks.
+    Validate the begin and end frames of the tracked cells.
 
     Begin frame (B) must be <= end frame (E), unless it is by a single frame.
     E can be one less than B if the cell existed but was never observed.
@@ -62,34 +62,34 @@ def validate_track_begin_end_frames(tracks: pd.DataFrame) -> None:
         return
 
     if not (tracks_begin_after_end["B"] - tracks_begin_after_end["E"] == 1).all():
-        msg = "The tracks start frame must be <= the tracks end frame, unless it is by a single frame."
+        msg = "The tracked cell's start frame must be <= the end frame, unless it is by a single frame."
         raise ValueError(msg)
 
 
 def validate_n_daughters(tracks: pd.DataFrame) -> None:
     """
-    Validate the number of daughters produced by each cell, as well as some other simple checks for the parent id.
+    Validate the number of daughters produced by each cell, as well as some other simple checks for the parent label.
 
     Each mother-cell should either have two daughters or none (right-censored or dying).
     """
-    # All parent ids (P) should match a track label (L) or be 0
+    # All parent labels (P) should match a cell label (L) or be 0
     parents_in_labels = tracks["P"].isin(tracks["L"])
     parents_are_zero = tracks["P"] == 0
     if not (parents_in_labels | parents_are_zero).all():
-        msg = "Track parents (fourth column) must match a track label (first column), or be set to zero"
+        msg = "Parent labels (fourth column) must match a cell label (first column), or be set to zero"
         raise ValueError(msg)
 
-    # The parent id (P) of a track can't be equal to its label (L)
+    # The parent label (P) of a cell can't be equal to its label (L)
     if (tracks["P"] == tracks["L"]).any():
-        msg = "A tracks parent (fourth column) can't be equal to its label (first column)"
+        msg = "A cell's parent (fourth column) can't be equal to its label (first column)"
         raise ValueError(msg)
 
-    # Each parent id should appear twice (i.e. two daughters), except for zero which can occur any number of times
+    # Each parent label should appear twice (i.e. two daughters), except for zero which can occur any number of times
     parent_counts = tracks["P"].value_counts()
     two_daughters = parent_counts == 2
     zero_label = parent_counts.index.to_series() == 0
     if not (two_daughters | zero_label).all():
-        msg = "Tracks must have 2 daughters, or None"
+        msg = "Cell must have 2 daughters, or None"
         raise ValueError(msg)
 
 
@@ -98,7 +98,7 @@ def validate_mother_daughter_frames(tracks: pd.DataFrame) -> None:
     # ignore parent ids of 0 (this means the parent is unknown)
     tracks_with_parents = tracks.loc[tracks["P"] != 0, :]
 
-    # For each track, its begin frame (B) must be one more than its parent's end frame (E)
+    # For each tracked cell, its begin frame (B) must be one more than its parent's end frame (E)
     tracks_with_parents = tracks_with_parents.merge(
         tracks[["L", "E"]], how="left", left_on="P", right_on="L", suffixes=("_track", "_parent")
     )
@@ -123,17 +123,17 @@ def preprocess_ctc_file(input_ctc_filepath: Path, output_ctc_filepath: Path) -> 
 
     Expects files in the standard CTC format, optionally with an additional column indicating right censoring.
     Columns are L B E P (R):
-    L - a unique label of the track (any positive number, not zero or negative)
-    B - the frame where the track begins (integer)
-    E - the frame where the track ends (integer)
-    P - label of the parent track (0 is used when the parent is unknown)
+    L - a unique label of the tracked cell (any positive number, not zero or negative)
+    B - the frame where the cell begins (integer)
+    E - the frame where the cell ends (integer)
+    P - label of the parent cell (0 is used when the parent is unknown)
     R - right censoring flag (1=right-censored, 0=not). Note a 0 only means it is not manually declared as
     right-censored. It will still be considered right-censored by the processing code, if the last observed frame
     coincides with the end of the movie.
     """
     tracks = pd.read_table(input_ctc_filepath, sep=r"\s+", header=None)
     validate_tracks_shape_dtypes(tracks)
-    validate_track_begin_end_frames(tracks)
+    validate_cell_begin_end_frames(tracks)
     validate_n_daughters(tracks)
     validate_mother_daughter_frames(tracks)
     validate_right_censored_daughters(tracks)
