75fcdb2687522b24bb2dd81fd5a9f7f3a6b7dc3a
Kimberly Meechan <24316371+K-Meech@users.noreply.github.com>
2024-10-11 14:37:49 +0100

    add test for btrack dead_track_ids

--- track_converter/tests/test_preprocess_btrack.py
+++ track_converter/tests/test_preprocess_btrack.py
@@ -14,20 +14,23 @@ def invalid_cell_labels():
     return (185, 177, 184)
 
 
-def test_btrack_using_terminate_fates(invalid_cell_labels, btrack_test_data_dir, tracks_out_path):
+@pytest.fixture
+def btrack_h5_path(btrack_test_data_dir):
+    return btrack_test_data_dir / "tracks.h5"
+
+
+def test_btrack_using_terminate_fates(invalid_cell_labels, btrack_h5_path, tracks_out_path):
     """
-    Run a valid example file through the pre-processing with use_terminate_fates on.
+    Run an example file through the pre-processing with use_terminate_fates on.
 
     This checks all parents / start / stop frames etc are set correctly, as well as all cells with terminate fates
     being assigned as right-censored.
     """
-    tracks_in_path = btrack_test_data_dir / "tracks.h5"
-
-    preprocess_btrack_file(tracks_in_path, tracks_out_path, use_terminate_fates=True, remove_false_positives=False)
+    preprocess_btrack_file(btrack_h5_path, tracks_out_path, use_terminate_fates=True, remove_false_positives=False)
     ctc_table = read_tracks(tracks_out_path)
 
     # Read tracks directly from file and check each appears properly in the CTC table
-    with HDF5FileHandler(tracks_in_path, "r") as reader:
+    with HDF5FileHandler(btrack_h5_path, "r") as reader:
         tracks = reader.tracks
 
     assert (len(tracks) - len(invalid_cell_labels)) == ctc_table.shape[0]
@@ -52,27 +55,52 @@ def test_btrack_using_terminate_fates(invalid_cell_labels, btrack_test_data_dir,
             assert track.ID not in ctc_table.L.to_numpy()
 
 
-def test_btrack_remove_false_positives(invalid_cell_labels, btrack_test_data_dir, tracks_out_path):
-    """
-    Run a valid example file through the pre-processing with remove_false_positives on.
-
-    This checks all false positive cells are removed.
-    """
-    tracks_in_path = btrack_test_data_dir / "tracks.h5"
-
-    preprocess_btrack_file(tracks_in_path, tracks_out_path, use_terminate_fates=True, remove_false_positives=True)
+def test_btrack_remove_false_positives(invalid_cell_labels, btrack_h5_path, tracks_out_path):
+    """Checks all cells with false positive fates are removed."""
+    preprocess_btrack_file(btrack_h5_path, tracks_out_path, use_terminate_fates=True, remove_false_positives=True)
     ctc_table = read_tracks(tracks_out_path)
     cell_labels = ctc_table.L.to_numpy()
 
     # Read tracks directly from file and check all tracks are present, except for false positives
-    with HDF5FileHandler(tracks_in_path, "r") as reader:
+    with HDF5FileHandler(btrack_h5_path, "r") as reader:
         tracks = reader.tracks
 
-    assert len(tracks) != ctc_table.shape[0]
     for track in tracks:
-        if track.fate == Fates.FALSE_POSITIVE:
+        if (track.fate == Fates.FALSE_POSITIVE) or (track.ID in invalid_cell_labels):
             assert track.ID not in cell_labels
-        elif track.ID not in invalid_cell_labels:
-            assert track.ID in cell_labels
         else:
-            assert track.ID not in cell_labels
+            assert track.ID in cell_labels
+
+
+def test_btrack_dead_track_ids(invalid_cell_labels, btrack_h5_path, tracks_out_path):
+    """Check all dead_track_ids are marked as not right-censored."""
+    dead_track_ids = [53, 49, 38]
+    preprocess_btrack_file(
+        btrack_h5_path,
+        tracks_out_path,
+        use_terminate_fates=False,
+        remove_false_positives=False,
+        dead_track_ids=dead_track_ids,
+    )
+    ctc_table = read_tracks(tracks_out_path)
+
+    # Read tracks directly from file and check all tracks with no children are marked as right-censored, except
+    # those in dead_track_ids
+    with HDF5FileHandler(btrack_h5_path, "r") as reader:
+        tracks = reader.tracks
+
+    for track in tracks:
+        if track.ID in invalid_cell_labels:
+            continue
+
+        right_censor = ctc_table.loc[ctc_table.L == track.ID, "R"].to_numpy()[0]
+
+        # dead track ids should not be right censored i.e. 0
+        if (len(track.children) == 0) and (track.ID in dead_track_ids):
+            assert right_censor == 0
+        # tracks with no children (that aren't marked as dead) should be right censored i.e. 1
+        elif (len(track.children) == 0) and (track.ID not in dead_track_ids):
+            assert right_censor == 1
+        # all other tracks aren't right censored i.e. 0
+        else:
+            assert right_censor == 0
